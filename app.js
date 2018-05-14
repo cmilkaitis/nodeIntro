bca6a9e9e6f10adee6842695b99e4e683b8b4e06
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-14 19:29:26 -0400

    Event Emitters

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-var stuff = require('./stuff');
+var events = require('events');
+/*
+Example 1
 
-console.log(stuff.counter(['chris', 'bob', 'jett']));
-console.log(stuff.adder(3,stuff.pi));
+var myEmitter = new events.EventEmitter();
+
+myEmitter.on('someEvent', function(msg){
+    console.log(msg);
+});
+
+
+myEmitter.emit('someEvent', 'the event was emitted');
+*/
+
+
+var util = require('util');
+
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('James');
+var mary = new Person('Mary');
+var ryu = new Person('Ryu');
+
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+    person.on("speak", function(msg){
+        console.log(`${person.name} said ${msg}`);
+    });
+});
+
+
+james.emit('speak', 'hey my ninjas');
