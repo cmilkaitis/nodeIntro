6ee27bbe80bd2d0fa0e2e2bf87ab012a1fcf49da
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-15 12:16:45 -0400

    Lesson16: Pipes

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 var http = require('http');
-/*
+var fs = require('fs');
+
 var server = http.createServer(function(request, response){
     console.log('request was made: ' + request.url);
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hey ninjazzz'); 
+    myReadStream.pipe(response);
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to port 3000');
-
-*/
-
-var fs = require('fs');
-
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
-
-myReadStream.on('data', function(chunk){
-    console.log('new chunk recieved.');
-    myWriteStream.write(chunk);
-   
-});
-
