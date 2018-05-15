e1fbc609e0f0bef08a95da9a8405bbf67b1a590e
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-15 13:05:20 -0400

    Lesson 19 - Basic Routing

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ var fs = require('fs');
 
 var server = http.createServer(function(request, response){
     console.log('request was made: ' + request.url);
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    var myObj = {
-        name: 'Ryu',
-        job : 'ninja',
-        age: 29
+    
+    if(request.url === '/home' || request.url === '/'){
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(response);
+    } else if (request.url === '/contact'){
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(response);
+    } else if (request.url === '/api/ninjas') {
+        var ninjas = [{name: 'ryu', age : 29}, {name: 'yoshi', age : 32}];
+        response.writeHead(200, {'Content-Type' : 'application/json'});
+        response.end(JSON.stringify(ninjas));
+    } else {
+        response.writeHead(404, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(response);
     }
-    response.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
