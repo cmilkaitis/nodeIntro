cfe14cde7dd86cd051758d2c0e53def733b2c9bd
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-15 11:39:20 -0400

    Readable streams

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var http = require('http');
-
+/*
 var server = http.createServer(function(request, response){
     console.log('request was made: ' + request.url);
     response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -9,14 +9,13 @@ var server = http.createServer(function(request, response){
 server.listen(3000, '127.0.0.1');
 console.log('listening to port 3000');
 
-
-//Lesson 13 - Streams and Buffers
-
-/*
-
-The example given of streaming a movie shows the concept of 
-streams (the path data take to get to the user) and buffers (the chuncks or slices of 
-accumlating data coming from the larger source) shows how breaking up data and 
-moving it in pieces is a preformance enhancer, over moving a file all at once
-
 */
+
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk recieved:');
+    console.log(chunk);
+});
