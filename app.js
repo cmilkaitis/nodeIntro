37f883269bb555ed6d00815189cbdafd650d5de5
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-15 11:24:28 -0400

    Lesson13: Streams and Buffers

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ var server = http.createServer(function(request, response){
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to port 3000');
+
+
+//Lesson 13 - Streams and Buffers
+
+/*
+
+The example given of streaming a movie shows the concept of 
+streams (the path data take to get to the user) and buffers (the chuncks or slices of 
+accumlating data coming from the larger source) shows how breaking up data and 
+moving it in pieces is a preformance enhancer, over moving a file all at once
+
+*/
