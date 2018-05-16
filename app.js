d240a7777ed2e70afa8864e405523b584ccc9a43
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-16 14:18:48 -0400

    Express Route Params

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ app.get('/contact', function(req, res){
     res.send('This is the contact page');
 });
 
+app.get('/profile/:id', function(req, res){
+    res.send('You requested to see a profile with the id of ' + req.params.id);
+})
+
 app.listen(3000);
 
