9159815217ae601e2194dacad1641d3dbb82eb3d
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-21 10:53:51 -0400

    Lesson 28 - Serving up static files with built in express middleware

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
     res.render('index');
